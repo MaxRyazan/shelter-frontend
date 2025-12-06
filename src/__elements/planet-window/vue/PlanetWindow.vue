ea945d8ddb8719eaa09fd79761810aa266857e94
maxryazan <no4ta87@mail.ru>
2025-12-06 23:15:10 +0300

    add login page

--- src/__elements/planet-window/vue/PlanetWindow.vue
+++ src/__elements/planet-window/vue/PlanetWindow.vue
@@ -3,18 +3,21 @@
                 @close="isPlanetWindowOpen = false">
         <template #header>
             <s-text :size="14" semi-bold>
-                Просмотр планеты
                 <span style="color: var(--accent-light)">
                     {{ currentPlanet?.name }}
                 </span>
             </s-text>
         </template>
+        <s-divider space="6"/>
+        <top-menu/>
     </drag-modal>
 </template>
 <script setup lang="ts">
 import DragModal from "@/components/modals/DragModal.vue";
 import {currentPlanet, isPlanetWindowOpen} from "@/__elements/planet-window/ts";
 import SText from "@/components/common/SText.vue";
+import TopMenu from "@/__elements/planet-window/vue/TopMenu.vue";
+import SDivider from "@/components/common/SDivider.vue";
 </script>
 <style scoped>
 .planet-window {
@@ -23,5 +26,14 @@ import SText from "@/components/common/SText.vue";
     @media screen and (max-width: 600px) {
         width: 360px !important;
     }
+}
+
+:deep(.dragmodal__inner) {
+    padding-top: 0;
+}
+
+:deep(.dragmodal__inner-header) {
+    display: flex;
+    padding-bottom: 0;
 }
 </style>
---
+++ src/__elements/planet-window/vue/TopMenu.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="top-menu">
+        <ul class="menu-list">
+            <li class="menu-list__item">
+                <s-text v-if="width > 600">обзор</s-text>
+                <common-icon v-else name="planet-icon.webp"/>
+            </li>
+            <li class="menu-list__item">
+                <s-text v-if="width > 600">склад</s-text>
+                <common-icon v-else name="planet-icon.webp"/>
+            </li>
+            <li class="menu-list__item">
+                <s-text v-if="width > 600">постройки</s-text>
+                <common-icon v-else name="planet-icon.webp"/>
+            </li>
+            <li class="menu-list__item">
+                <s-text v-if="width > 600">очередь</s-text>
+                <common-icon v-else name="planet-icon.webp"/>
+            </li>
+            <li class="menu-list__item">
+                <s-text v-if="width > 600">производства</s-text>
+                <common-icon v-else name="planet-icon.webp"/>
+            </li>
+            <li class="menu-list__item">
+                <s-text v-if="width > 600">оборона</s-text>
+                <common-icon v-else name="planet-icon.webp"/>
+            </li>
+        </ul>
+    </div>
+</template>
+<script setup lang="ts">
+import SText from "@/components/common/SText.vue";
+import CommonIcon from "@/components/icons/CommonIcon.vue";
+import {useWindowSize} from '@vueuse/core';
+
+
+const {width} = useWindowSize()
+
+</script>
+<style scoped>
+.top-menu {
+    width: 100%;
+
+}
+
+.menu-list {
+    display: flex;
+    justify-content: space-between;
+    gap: 10px;
+    margin: 0;
+    list-style-type: none;
+}
+
+.menu-list__item {
+    margin: 0;
+    font-family: IBM_Plex_Mono sans-serif;
+    font-size: 14px;
+    font-weight: 700;
+}
+</style>
