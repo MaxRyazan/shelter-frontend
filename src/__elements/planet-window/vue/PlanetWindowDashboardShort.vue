d4c1f32acfc48131590affeea9e251e84fea37f3
maxryazan <no4ta87@mail.ru>
2025-12-27 20:38:16 +0300

    add buildings

--- src/__elements/planet-window/vue/PlanetWindowDashboardShort.vue
+++ src/__elements/planet-window/vue/PlanetWindowDashboardShort.vue
@@ -4,8 +4,14 @@
             <s-text class="dashboard-short__item-line">
                 склад
             </s-text>
-            <s-text class="dashboard-short__item-line">{{ $rs(toNum(currentPlanet?.storage?.capacity?.currentFill), 3) }}</s-text>
-            <s-text class="dashboard-short__item-line">{{ $rs(toNum(currentPlanet?.storage?.capacity?.maxCapacity), 3) }}</s-text>
+            <s-text class="dashboard-short__item-line">{{
+                    $rs(toNum(currentPlanet?.storage?.capacity?.currentFill), 3)
+                }}
+            </s-text>
+            <s-text class="dashboard-short__item-line">{{
+                    $rs(toNum(currentPlanet?.storage?.capacity?.maxCapacity), 3)
+                }}
+            </s-text>
             <s-text
                 :positive="storageCapacityBalance > 0"
                 :negative="storageCapacityBalance < 0"
@@ -43,8 +49,8 @@
             <s-text class="dashboard-short__item-line">
                 провиант
             </s-text>
-            <s-text class="dashboard-short__item-line">{{ $rs(toNum(food?.incoming) , 3) }}</s-text>
-            <s-text class="dashboard-short__item-line">{{ $rs(toNum(food?.incoming) , 3) }}</s-text>
+            <s-text class="dashboard-short__item-line">{{ $rs(toNum(food?.incoming), 3) }}</s-text>
+            <s-text class="dashboard-short__item-line">{{ $rs(toNum(food?.incoming), 3) }}</s-text>
             <s-text
                 :positive="toNum(food?.balance) > 0"
                 :negative="toNum(food?.balance) < 0"
@@ -64,13 +70,13 @@
                     class="dashboard-short__icon"
                     v-if="toNum(people?.birthRate) > 0"/>
             </s-text>
-            <s-text class="dashboard-short__item-line">{{ $rs(toNum(people?.currentCitizens ), 3) }}</s-text>
+            <s-text class="dashboard-short__item-line">{{ $rs(toNum(people?.currentCitizens), 3) }}</s-text>
             <s-text class="dashboard-short__item-line">{{ $rs(toNum(people?.consuming), 3) }}</s-text>
             <s-text
-                :positive="toNum(people?.balance) > 0"
+                :positive="(toNum(people?.currentCitizens) - toNum(people?.consuming)) > 0"
                 :negative="toNum(people?.balance) < 0"
                 class="dashboard-short__item-line">
-                {{ $rs(toNum(people?.balance), 3) }}
+                {{ $rs((toNum(people?.currentCitizens) - toNum(people?.consuming)), 3) }}
             </s-text>
         </div>
     </div>
